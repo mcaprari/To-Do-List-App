eb8a4764e4c9c257a70e1e796ab53ea37df93eef
mcaprari <caprarimilan@gmail.com>
2023-03-10 18:02:25 -0500

    Task 1.9 corrections
    - finished app

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,29 @@
-let li = $('<li></li>');
-  let inputValue = $('#input').val();
-  li.append(inputValue);
+function newItem() {
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
 
-  if (inputValue === '') {
+    if (inputValue === '') {
     alert("You must write something!");
-  } else {
+    } else {
     $('#list').append(li);
-  }
+    }
 
-  function crossOut() {
-		li.toggleClass("strike");
-	}
+    function crossOut() {
+        li.toggleClass("strike");
+    }
 
-	li.on("dblclick", function crossOut() {
-		li.toggleClass("strike");
-	});
+    li.on("dblclick", function crossOut() {
+        li.toggleClass("strike");
+    });
 
-  let crossOutButton = $('<crossOutButton></crossOutButton>');
-  crossOutButton.append(document.createTextNode('X'));
-  li.append(crossOutButton);
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
 
-  crossOutButton.on("click", deleteListItem);
-  function deleteListItem(){
-		li.addClass("delete")
-	}
-   $('#list').sortable();
-
+    crossOutButton.on("click", deleteListItem);
+    function deleteListItem(){
+        li.addClass("delete")
+    }
+    $('#list').sortable();
+}
